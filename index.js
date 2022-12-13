0feb8fb761fe1da8de92c423580e3c0c589b442a
alifshah <alifhasanshah@gmail.com>
2022-12-14 05:19:00 +0600

    image generated

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const openaiRoutes = require('./routes/openaiRoutes')
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000 ;
 
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 app.use('/openai', openaiRoutes)
 
