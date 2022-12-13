e9d3d9039e5fbc0b730ec2aa0000b9ce8c2c08b3
alifshah <alifhasanshah@gmail.com>
2022-12-14 05:28:07 +0600

    ai image generate automatically

--- controllers/openaiControllers.js
+++ controllers/openaiControllers.js
@@ -7,11 +7,13 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req, res) => {
+    const {prompt, size} = req.body;
+    const imageSize = size ==='small'? '256x256' : size === 'medium' ? '512x512' : '1024x1024'
     try {
         const response = await openai.createImage({
-            prompt: "frog on a computer",
+            prompt,
             n: 1,
-            size: '512x512',
+            size: imageSize,
         });
         const imageUrl = response.data.data[0].url;
         res.status(200).json({
