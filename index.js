95b5dde968aa19fe9df287eb56c2348e15b50468
alifshah <alifhasanshah@gmail.com>
2022-12-13 21:57:54 +0600

    routing complete

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const openaiRoutes = require('./routes/openaiRoutes')
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000 ;
 
 const app = express();
+
+app.use('/openai', openaiRoutes)
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
